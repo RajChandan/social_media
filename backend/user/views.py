79531fcf8b86c763d1435c372684f16d0ba0cbae
RajChandan <raj.chandan91@gmail.com>
2025-01-17 00:37:36 +0530

    user register view

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets,status
+from rest_framework.decorators import api_view,action,permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer,RegisterSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"user_registered succesfully"},status = status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
