7fc6b79e91cc940ae44d3caf594d614d7ac84295
RajChandan <raj.chandan91@gmail.com>
2025-01-23 23:51:02 +0530

    user serializer follow

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -5,19 +5,21 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+    followers = serializers.StringRelatedField(many=True)  # Optional: Return usernames
+    following = serializers.StringRelatedField(many=True)  # Optional: Return usernames
+
     class Meta:
         model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "first_name",
-            "last_name",
-            "bio",
-            "profile_picture",
-            "followers",
-        ]
-        read_only_fields = ["followers"]
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following', 'followers_count',
+                  'following_count']
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
