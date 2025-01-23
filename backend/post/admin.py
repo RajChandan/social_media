bd713734c02dd1cc5ae16a8b2336b2b2a0bce807
RajChandan <raj.chandan91@gmail.com>
2025-01-24 02:04:26 +0530

    comment model

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post,Comment
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["author","content"]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["post","author","content"]
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
--- backend/post/models.py
+++ backend/post/models.py
@@ -24,3 +24,12 @@ class Post(models.Model):
         return self.likes.count()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content} by USER {self.author} on {self.post}"
+
