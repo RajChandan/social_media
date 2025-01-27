603dc1aa6e58bd027b95927e84e1c5c4d99e00c2
RajChandan <raj.chandan91@gmail.com>
2025-01-27 23:31:46 +0530

    post model serializer

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,2 +1,18 @@
 from rest_framework import serializers
 from .models import Post,Comment
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id","author","content","created_at"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    comment = CommentSerializer(many=True,read_only=True)
+    like_count = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Post
+        fields = ["id","author","content","image","created_at","like_count","comments"]
