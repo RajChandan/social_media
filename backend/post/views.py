edce4be27c03dc63ac485add10d19ec447425fbf
RajChandan <raj.chandan91@gmail.com>
2025-01-27 23:46:14 +0530

    post view initialization

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets,status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post,Comment
+from .serializers import CommentSerializer,PostSerializer
 # Create your views here.
