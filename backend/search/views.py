f89f19e09c9a633bab0ea1a2463414c4af21c3dd
RajChandan <raj.chandan91@gmail.com>
2025-02-01 23:42:06 +0530

    search viewset

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from  .documents import PostDocument
+import requests
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
-def search_post(request):
-    return JsonResponse({"result":[]})
+class SearchView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        query = requests.GET.get("q","").strip()
+        if not query:
+            return Response({"error":"no query to search"})
+
+        es_url = "http://localhost:9200/posts/_search"
+        payload = {
+            "query" : {
+                "multi_match" : {
+                    "query" : query,
+                    "fields" : ["content","author.username"]
+                }
+            }
+        }
+
+        try:
+            response = requests.get(es_url,json=payload)
+            data = response.json()
+            return Response(data,status = response.status_code)
+        except requests.exceptions.RequestException as e:
+            return Response({"error":str(e)},status=500)
+
+
