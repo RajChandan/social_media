8f94ba92c0b715e1dad21eca771f67ad0cb573b2
RajChandan <raj.chandan91@gmail.com>
2025-02-04 23:18:13 +0530

    css tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure Tailwind works in React components
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
