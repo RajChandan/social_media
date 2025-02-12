b7aba6b12e3c96bedaa236cec2bd886166edf655
RajChandan <raj.chandan91@gmail.com>
2025-02-12 19:15:53 +0530

    profile picture in register page

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:8000";
 
-export const registerUser = async (userdata) => {
+export const registerUser = async (formData) => {
     try
     {
-        const response = await axios.post(`${API_URL}/user/register/`,JSON.stringify(userdata),
+        const response = await axios.post(`${API_URL}/user/register/`,formData,
         {
-            headers : {"Content-Type":"application/json"}
+            headers : {"Content-Type":"multipart/form-data"}
         });
         return response.data;
 
--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -4,7 +4,7 @@ import {useNavigate} from "react-router-dom";
 
 const Register = () =>
 {
-    const [formData,setFormData] = useState({username:"",email:"",password:"",first_name:"",last_name:"",bio:""});
+    const [formData,setFormData] = useState({username:"",email:"",password:"",first_name:"",last_name:"",bio:"", profile_picture:null});
     const [error,setError] = useState(null);
     const [success,setSuccess] = useState(null);
     //const navigate =   useNavigate();
@@ -14,12 +14,31 @@ const Register = () =>
         setFormData({...formData,[e.target.name] : e.target.value});
     };
 
+    const handleFileChange = (e) =>
+    {
+        setFormData({...formData,profile_picture:e.target.files[0]});
+    }
+
     const handleSubmit = async(e) =>
     {
         e.preventDefault();
+        setError(null);
+        setSuccess(null);
         try
         {
-            await registerUser(formData);
+            const formDataToSend = new FormData();
+            formDataToSend.append("username" , formData.username);
+            formDataToSend.append("email",formData.email);
+            formDataToSend.append("password",formData.password);
+            formDataToSend.append("first_name",formData.first_name);
+            formDataToSend.append("last_name",formData.last_name)
+            formDataToSend.append("bio",formData.bio);
+
+            if (formData.profile_picture)
+            {
+                formDataToSend.append("profile_picture",formData.profile_picture);
+            }
+            await registerUser(formDataToSend);
             setSuccess("User created Successfully !!")
             //navigate("/login");
         }
@@ -43,7 +62,7 @@ const Register = () =>
                 <input type="text" name="last_name" placeholder="last name" className="w-full p-2 mb-2 border rounded" onChange={handleChange} required/>
                 <textarea name="bio" placeholder="Bio" className="w-full p-2 mb-2 border rounded" onChange={handleChange} />
                 <input type="password" name="password" placeholder="Password" className="w-full p-2 mb-2 border rounded" onChange={handleChange} required/>
-                <input type="file" name="profile_picture" accept="image/*" className="w-full p-2 mb-2 border rounded"/>
+                <input type="file" name="profile_picture" accept="image/*" className="w-full p-2 mb-2 border rounded" onChange={handleFileChange}/>
                 <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Register</button>
             </form>
             
