569c97c7685e6a0bf546ac31e77064949caec088
RajChandan <raj.chandan91@gmail.com>
2025-02-15 20:51:54 +0530

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/register";
 import Login from "./pages/login";
+import Profile from "./pages/profile";
+import ProtectedRoute from "./components/protectedroute";
+
 function App() {
   return (<Router>
     <Routes>
       <Route path="/register" element={<Register />}></Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/profile" element={<ProtectedRoute><Profile />
+      </ProtectedRoute>}>
+      </Route>
     </Routes>
   </Router>);
 }
