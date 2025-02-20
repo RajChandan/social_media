a785654cdd566566ebc785c76df773ac8251fa98
RajChandan <raj.chandan91@gmail.com>
2025-02-20 21:19:54 +0530

    user model update

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,3 +1,6 @@
+from importlib.metadata import requires
+
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -12,8 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following', 'followers_count',
-                  'following_count']
+        fields = [
+            "id",
+            "username",
+            "email",
+            "bio",
+            "profile_picture",
+            "followers",
+            "following",
+            "followers_count",
+            "following_count",
+        ]
 
     def get_followers_count(self, obj):
         return obj.followers.count()
@@ -21,14 +33,16 @@ class UserSerializer(serializers.ModelSerializer):
     def get_following_count(self, obj):
         return obj.following.count()
 
-    def get_profile_picture(self,obj):
+    def get_profile_picture(self, obj):
         request = self.context.get("request")
         if obj.profile_picture:
             return request.build_absolute_uri(obj.profile_picture.url)
         return "https://avatar.iran.liara.run/public/boy"
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+    github_id = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
@@ -40,18 +54,32 @@ class RegisterSerializer(serializers.ModelSerializer):
             "last_name",
             "bio",
             "profile_picture",
-
-
+            "github_id",
         ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            password=validated_data["password"],
-            first_name=validated_data.get("first_name", ""),
-            last_name=validated_data.get("last_name", ""),
-            bio=validated_data.get("bio", ""),
-            profile_picture=validated_data.get("profile_picture", None),
-        )
+        github_id = validated_data.pop("github_id", None)
+        if github_id:
+            user, created = User.objects.get_or_create(
+                email=validated_data["email"],
+                defaults={
+                    "username": validated_data.get("username"),
+                    "first_name": validated_data.get("first_name", ""),
+                    "last_name": validated_data.get("last_name", ""),
+                    "bio": validated_data.get("bio", ""),
+                    "profile_picture": validated_data.get("profile_picture", None),
+                },
+            )
+
+        else:
+            user = User.objects.create_user(
+                username=validated_data["username"],
+                email=validated_data["email"],
+                password=validated_data["password"],
+                first_name=validated_data.get("first_name", ""),
+                last_name=validated_data.get("last_name", ""),
+                bio=validated_data.get("bio", ""),
+                profile_picture=validated_data.get("profile_picture", None),
+            )
+
         return user
